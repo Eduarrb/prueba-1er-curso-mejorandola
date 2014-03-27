2bb5b737977eb380ac5049d31533f3946b1fe3b4
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2014-03-27 10:48:31 -0500

    cambios

--- formulario.js
+++ formulario.js
@@ -1,14 +1,29 @@
-var $form = $('#formulario'),
+var $form 	= $('#formulario'),
 	$titulo = $('#titulo'),
-	$url = $('#url'),
+	$url 	= $('#url'),
 	$button = $('#mostrar-form'),
-	$list = $('#contenido'),
-	$post = $('.item').first();
+	$list 	= $('#contenido'),
+	$post 	= $('.item').first();
+
+	ls		= localStorage,
+	ss 		= sessionStorage;
+//guarda en la memoria los contenidos y se peude recargar 
+//la página sin perder lo ya escrito antes de enviar
+if(ls.getItem('autosave')){
+	$titulo.val(ss.getItem('titulo'));
+	$url.val(ss.getItem('url'));
+}
+
+var id = setInterval(function(){
+	ss.setItem('titulo',$titulo.val());
+	ss.setItem('url',$url.val());
+},1000);
+
 
 function mostrarFormulario(){
 	$form.slideToggle();
 	$list.slideToggle();
-	// $('aside').hide();
+	$('video').slideToggle();
 	return false;
 }
 
@@ -16,24 +31,42 @@ function agregarPost(e){
 	e.preventDefault();
 	var url = $url.val(),//val() coje al valor dentro del texto
 		titulo = $titulo.val(),
-		$clone = $post.clone();
+		clone = $post.clone();
 		
-	$clone.find('.titulo_item a')
+	clone.find('.titulo_item a')
 		.text(titulo)
 		.attr('href', url);
 
-	$clone.hide();
+	clone.hide();
 
-	$list.prepend($clone);//pone el clon al inicio del contenido
+	$list.prepend(clone);//pone el clon al inicio del contenido
 	mostrarFormulario(); // muestra la lista
 
 	$titulo.val(""); //pone en blanco los parametros ya puestos
 	$url.val("");
 
-	$clone.fadeIn();
+	clone.fadeIn();
 	// return false //ya no se necesita con preventDefault
 }
+// esta funcion sirve para guardar datos en la pc local
+function grabarInformacion(e){
+	e.preventDefault();
+	var url = $url.val(),
+		titulo = $titulo.val(),
+		ls = localStorage,
+		ss = sessionStorage;
+
+	ls.setItem('titulo', titulo);
+	ls.setItem('url', url);
+
+	ss.setItem('titulo', titulo);
+	ss.setItem('url', url);
+
+	mostrarFormulario(); 
+	$titulo.val(""); 
+	$url.val("");
+};
 
 	// Eventos
 $button.click(mostrarFormulario);
-$form.on('submit', agregarPost);
+$form.on('submit', /*grabarInformacion*/ agregarPost);
